0355e59e3b9b3e5fe1b7920326bf95e2d66d11f5
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-18 18:55:39 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,7 +2,7 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 BRANDS = []
-def initialize(brand)
+def initialize(brand,key)
     @brand = brand
     if BRANDS[key] =! brand
     BRANDS << brand
