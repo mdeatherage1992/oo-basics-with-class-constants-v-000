97bf9a31d71a059c99950b25d3afdcaa7496fdf0
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-18 18:57:14 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -4,12 +4,8 @@ class Shoe
 BRANDS = []
 def initialize(brand)
     @brand = brand
-    BRANDS.each do |brand|
-      if BRANDS.exclude? brand
     BRANDS << brand
   end
-end
-end
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
